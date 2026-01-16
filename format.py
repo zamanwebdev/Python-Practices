257872afffc908344a9a3acf5c015b154d173daf
Syed Zaman <zamanwebdev@gmail.com>
2026-01-16 21:57:51 +0600

    More Practice Done of this Day One.

---
+++ format.py
@@ -0,0 +1,7 @@
+Num1 = 20
+Num2 = 50
+print("This is my Super Number", Num1 + Num2)
+print(f"This is my Super Number {Num1 + Num2}")
+Username = "Zaman"
+Roll = 9
+print(f"My Name is {Username} & My Roll Number is {Roll} ")
