67f82e26618fa475b14faad3d5f4d3892ba85f17
Syed Zaman <zamanwebdev@gmail.com>
2026-01-16 21:29:09 +0600

    #8 Python String Type Data Practice Done.

--- DayOnePractice.py
+++ DayOnePractice.py
@@ -1,6 +1,7 @@
 print("Hello World!")
 print('Hello Zaman!')
 print("'Hello Mourosi!'")
+# This is Single Line comments
 '''
 This is
 Multi 
@@ -8,8 +9,19 @@ Line
 Comments 
 '''
 """
-This is also
-Multi 
-Line 
-Comments 
+This is also Multi 
+Line Comments 
 """
+# int type data
+integer = 294923498238
+print(type(integer))
+# float type data
+float = 1.2
+print(type(float))
+# complex type data
+complex = 234214323j
+print(type(complex))
+# str type data
+FirstName = "Syed Zaman"
+LastName = "Mostafiz"
+print("My Name is" + " " + FirstName + " " + LastName)
