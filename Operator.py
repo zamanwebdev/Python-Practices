e4f7c8fbeee853ab8e2c97b746302d7f9e2fac06
Syed Zaman <zamanwebdev@gmail.com>
2026-01-17 20:16:12 +0600

    #13 Python Operator Practice Done.

---
+++ Operator.py
@@ -0,0 +1,19 @@
+# Python Arithmetic Operators
+a = 20
+b = 10
+# Addition Arithmetic Operators
+print(a + b)
+# Subtraction Arithmetic Operators
+print(a - b)
+# Multiplication Arithmetic Operators
+print(a * b)
+# Division Arithmetic Operators
+print(a / b)
+# Modulus Arithmetic Operators
+print(a % b)
+# Exponentiation Arithmetic Operators
+c = 5
+d = 3
+print(c ** d) # 5*5*5
+# Floor division Arithmetic Operators
+print(a // b)
