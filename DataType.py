e9b6208a45bcbaf2c3f7677eb069f96b874a24ef
Syed Zaman <zamanwebdev@gmail.com>
2026-01-17 19:42:07 +0600

    #12 Sequence Type Data In Python Practice Done.

--- DataType.py
+++ DataType.py
@@ -2,3 +2,17 @@
 x = None
 print(x)
 print(type(x))
+# Sequence Type Data
+# List Type Data
+li = ['Zaman', 'Zahid', 'Sumon', 'Pintu']
+print(type(li))
+print(li[0])
+li[0] = "Mostafiz"
+print(li)
+# tuple type data
+tup = (5,10,15,20,25,30,35,40)
+print(tup)
+# Range Type Data
+ran = range(6)
+for i in ran:
+    print(i)
