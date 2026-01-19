ea0e240241496ea6abcf9cd93a460981521050bb
Syed Zaman <zamanwebdev@gmail.com>
2026-01-19 22:59:04 +0600

    #18 Python List Data Practice Done.

---
+++ myList.py
@@ -0,0 +1,11 @@
+li = [1,2,3]
+print(li)
+print(type(li))
+li[0] = 10
+print(li[0])
+lis = ['Zaman', 'Zahid', 'Sumon', 'Pintu']
+print(lis)
+print(type(lis))
+list = [True, False, True]
+print(list)
+print(type(list))
