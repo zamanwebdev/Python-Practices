1a8a49841389123da3d0827cd1c56b39ee2a74aa
Syed Zaman <zamanwebdev@gmail.com>
2026-01-19 12:18:28 +0600

    #14 Python Assignment & Comparison Operator Practice Done.

--- Operator.py
+++ Operator.py
@@ -17,3 +17,45 @@ d = 3
 print(c ** d) # 5*5*5
 # Floor division Arithmetic Operators
 print(a // b)
+# Python Assignment Operators
+e = 5
+e+=3 # Addition Assignment Operators
+print(e)
+f = 10
+f-= 5 # Subtraction Assignment Operators
+print(f)
+
+G = 10
+G*= 5
+print(G) # Multiplication Assignment Operators
+print(f)
+
+H = 10
+H/= 5 # Division Assignment Operators
+print(H)
+
+H2 = 10
+H2%= 5 # Modulus Assignment Operators
+print(H)
+
+
+
+# Python Comparison Operators
+x = 5
+y = 3
+# (==) Equal Comparison Operators
+print(x == y) # returns False because 5 is not equal to 3
+# (!=) Not equal Comparison Operators
+print(x != y) # returns True because 5 is not equal to 3
+# (>) Greater than Comparison Operators
+print(x > y)  # returns True because 5 is greater than 3
+# (<) Less than Comparison Operators
+print(x < y) # returns False because 5 is not less than 3
+# (>=) Greater than or equal to - Comparison Operators
+print(x >= y) # returns True because five is greater, or equal, to 3
+# (<=) Less than or equal to Comparison Operators
+print(x <= y) # returns False because 5 is neither less than or equal to 3
+
+
+# Logical Operators
+
