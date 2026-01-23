2102d7e68e1e9e14d92192d9614c76a0b605df2d
Syed Zaman <zamanwebdev@gmail.com>
2026-01-23 22:20:11 +0600

    #22 Python Loop List Practice Done.

---
+++ Practice22.py
@@ -0,0 +1,14 @@
+# Python - Loop Lists
+# You can loop through the list items by using a for loop:
+LoopList = ["mahi","sabnnur","bobita","porimoni","Mahjabin"]
+for Prem in LoopList:
+    print(Prem)
+# Use the range() and len() functions to create a suitable iterable.
+for i in range(len(LoopList)):
+    print(i)
+# Print all items, using a while loop to go through all the index numbers
+y = 0
+while y < len(LoopList):
+    print(LoopList[y])
+    y = y + 1
+
