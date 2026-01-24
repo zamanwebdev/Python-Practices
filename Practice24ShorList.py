4ed331cfe1100a4ff0f84261414f1aa27164da36
Syed Zaman <zamanwebdev@gmail.com>
2026-01-24 23:15:32 +0600

    #24 Python Sort List Practice Done.

---
+++ Practice24ShorList.py
@@ -0,0 +1,14 @@
+# Sort List
+Number = [9,7,5,3,1,2,4,6,8]
+Number.sort()
+print(Number)
+Number.sort(reverse=True)
+print(Number)
+
+eng = ["d", "b", "a","c","f","e"]
+eng.sort()
+print(eng)
+eng.sort(reverse=True)
+print(eng)
+
+
