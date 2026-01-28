a8afa8d472ba3fb1eabef071a7abdd17db57ea4e
Syed Zaman <zamanwebdev@gmail.com>
2026-01-28 21:17:20 +0600

    #35 Python Tuple Method Practice Done.

---
+++ #35TupleMethod.py
@@ -0,0 +1,8 @@
+#35 Python Tuple Method
+fruits = ("Apple", "Apple", "Cherry", "Apple", "Elderberry", "Fig", "Grapes", "Honeydew", 100)
+# indexing = fruits.index("Fig")
+indexing = fruits.index(100)
+print(indexing)
+C = fruits.count("Apple")
+print(C)
+
