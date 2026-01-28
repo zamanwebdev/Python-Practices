f2162d63f3fe6499931722adbdd7db5e6b536388
Syed Zaman <zamanwebdev@gmail.com>
2026-01-28 21:03:50 +0600

    #34 Python Join Tuple Practice Done.

---
+++ #33loopTuple.py
@@ -0,0 +1,10 @@
+#33 Python loop Tuple
+fruits = ("Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grapes", "Honeydew")
+# for i in fruits:
+#     print(i)
+# for x in range(len(fruits)):
+#     print(fruits[x])
+i = 1
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
---
+++ #34JoinTuple.py
@@ -0,0 +1,6 @@
+#34 Python Join Tuple
+Tuple1 = ("a","b","c")
+Tuple2 = (1,2,3)
+Tuple3 = Tuple1 + Tuple2
+print(Tuple3)
+print(Tuple3 * 2)
