1a925b7b95f73ac75e7909c3e0823d3558aae837
Syed Zaman <zamanwebdev@gmail.com>
2026-01-29 16:26:19 +0600

    #36 Python Tuple Exercise Practice Done.

---
+++ #36TupleExercise.py
@@ -0,0 +1,13 @@
+# Python Tuple Exercise
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+# Use the correct syntax to print the first item in the fruits tuple.
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+# Use negative indexing to print the last item in the tuple.
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+# Use a range of indexes to print the third, fourth, and fifth item in the tuple.
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
