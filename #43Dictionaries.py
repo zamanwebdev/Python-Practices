ab67015647d536d92044dbc7c258fd24a5d190f3
Syed Zaman <zamanwebdev@gmail.com>
2026-01-31 23:56:40 +0600

    #43 Python Dictionaries Practice Done.

---
+++ #43Dictionaries.py
@@ -0,0 +1,17 @@
+studentInfo = {
+    "Zaman" : {
+        "name" : "Zaman",
+        "age" : 22,
+        "address" : "My city",
+        "Roll" : "370026",
+        "email" : "zamanwebdev@gmail.com"
+    },
+    "Zahid" :{
+        "name" : "Zahid",
+        "age" : 22,
+        "address" : "My city",
+        "Roll" : "370020",
+        "email" : "zahid@gmai.com"
+    }
+}
+print(studentInfo["Zahid"]["age"])
