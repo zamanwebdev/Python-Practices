6eae404a18f98cafb0ce6fa4e692d225801ab2c6
Syed Zaman <zamanwebdev@gmail.com>
2026-01-31 15:32:07 +0600

    #42.2 Python Set Item Exercise Practice Done.

---
+++ #42.2SetItemExercise.py
@@ -0,0 +1,18 @@
+# Python Set add() Method
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+# Python Set clear() Method
+fruits = {"apple", "banana", "cherry"}
+fruits.clear()
+print(fruits)
+# Set copy() Method
+x = fruits.copy()
+print(x)
+# Set difference() Method
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.difference(y)
+diff2 = y.difference(x)
+print(z)
+print(diff2)
