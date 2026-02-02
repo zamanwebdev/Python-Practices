287ccbd9c86b117e94dc4be9c5fded1374f9a8c3
Syed Zaman <zamanwebdev@gmail.com>
2026-02-02 23:38:16 +0600

    #52 Python Break & Continue Keyword Practice Done.

---
+++ #52BreakContinue.py
@@ -0,0 +1,7 @@
+#52 Python Break & Continue
+HabluLlist = [1,2,3,4,5,6,7,8,9]
+HabluLlist = ["Zaman", "Zahid","Sumon"]
+for x in range(len(HabluLlist)):
+    if x == 2:
+        break
+    print(HabluLlist[x])
