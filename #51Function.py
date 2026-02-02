50c756cdaf343385db6fa9e4eb1d55390c5fb9c7
Syed Zaman <zamanwebdev@gmail.com>
2026-02-02 23:26:08 +0600

    #51 Python Function Practice Done.

---
+++ #51Function.py
@@ -0,0 +1,17 @@
+#51 Python Function
+def my_function():
+    print("Hello from a function")
+my_function()
+
+def HabluFun(a,b,c):
+    sum = a + b + c
+    print(sum)
+HabluFun(4,2,7)
+HabluFun(5,3,9)
+def minus(a,b):
+    return a - b
+print(minus(6,5))
+def multi(x,z):
+    multi = x * z
+    print(multi)
+multi(2,3)
