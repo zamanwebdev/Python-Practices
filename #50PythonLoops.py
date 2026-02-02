339b6e3cebf192795285e062565aadeec89d0a61
Syed Zaman <zamanwebdev@gmail.com>
2026-02-02 23:02:30 +0600

    #50 Python Loops Practice Done.

---
+++ #50PythonLoops.py
@@ -0,0 +1,13 @@
+for x in range(2,6):
+    print(x)
+
+hablu = 0
+while hablu < 10:
+    print("yes hablu is less than ",hablu)
+    hablu += 1
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
