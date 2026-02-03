a4e1e4a5a81712c829aa474168adfdb1dc4b481a
Syed Zaman <zamanwebdev@gmail.com>
2026-02-03 16:17:08 +0600

    #56 Python Lambda Functions Practice Done.

--- #54Zip.py
+++ #54Zip.py
@@ -1,4 +1,9 @@
+#54 Python Zip  Functions
 
 list1 = ["Zaman", "Zahid","Sumon"]
 list2 = ["Python Programmer","Networking","Php Programmer"]
 print(list(zip(list1,list2)))
+
+#56 Python Lambda Functions
+x = lambda a,b,c,d: (a+b) / (c+d)
+print(x(10,8,6,4))
