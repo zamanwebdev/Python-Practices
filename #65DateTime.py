16a2e2ae470e5f283f566de270bb346011fb38ed
Syed Zaman <zamanwebdev@gmail.com>
2026-02-03 20:40:28 +0600

    #65 Python DateTime Practice Done.

---
+++ #65DateTime.py
@@ -0,0 +1,9 @@
+#65 Python DateTime
+import datetime
+a = datetime.datetime.now()
+print(a.strftime("%a"))
+print(a.strftime("%A"))
+print(a.strftime("%d%m%y"))
+print(a.strftime("%c"))
+print(a.strftime("%X"))
+print(a.strftime("%m"))
