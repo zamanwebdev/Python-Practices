d5c16a3bc1d6a591cc99e1aa6e51204deeb22a1a
Syed Zaman <zamanwebdev@gmail.com>
2026-02-04 21:06:05 +0600

    #63 Python Iterator Practice Done

---
+++ #63Iterator.py
@@ -0,0 +1,16 @@
+#63 Python Iterator
+
+list = [1,2,3,4,5, "Zaman", "Zahid", "Sumon"]
+
+# for i in list:
+#     print(i)
+
+x = iter(list)
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
