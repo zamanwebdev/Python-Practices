1a68dfcb169898a8c1f21f0f9db36f84c7322e75
Syed Zaman <zamanwebdev@gmail.com>
2026-02-04 19:54:24 +0600

    #58 Python Class & Object Practice Done.

--- #58ClassObject.py
+++ #58ClassObject.py
@@ -1,8 +1,14 @@
 class Hablu:
     Name = ""
     Number = ""
+    roll = ""
 a = Hablu()
 a.Name = "Syed Zaman"
 a.Number = "123456"
 print(a.Name)
 print(a.Number)
+b = Hablu()
+b.Name = "Zahid"
+b.Number = "123456"
+print(b.Name)
+print(b.Number)
