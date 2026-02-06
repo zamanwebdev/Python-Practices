26ec22ad72acf87896216843ec96ca2c97d74b93
Syed Zaman <zamanwebdev@gmail.com>
2026-02-06 10:28:24 +0600

    #74 Python Constructor Practice Done.

---
+++ #74.2_Constructor.py
@@ -0,0 +1,5 @@
+# Parameterized Constructor
+class parentInfo:
+    def __init__(self,name,Number):
+        print(f"My name is {name}, & My Number is {Number}")
+p1 = parentInfo("Zaman","01518313365")
---
+++ #74_Constructor.py
@@ -0,0 +1,8 @@
+#74 Python Constructor
+class parentInfo:
+    def ZamanFamily(self,name,age):
+        print(f"My name is {name} and I am {age} years old")
+
+p1 = parentInfo()
+p1.ZamanFamily("Zaman",30)
+p1.ZamanFamily("Habu",91)
